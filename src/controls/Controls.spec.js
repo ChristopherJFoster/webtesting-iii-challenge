610e2fbadf30bfd23a6e2bce4e699277bf35e4a2
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-04-10 16:37:14 -0400

    Added basic snapshot test for each of the 3 components.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,16 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+
+import Controls from './Controls';
+
+afterEach(() => {
+  cleanup();
+});
+
+describe('<Controls />', () => {
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Controls />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
-// Test away
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+
+import Dashboard from './Dashboard';
+
+afterEach(() => {
+  cleanup();
+});
+
+describe('<Dashboard />', () => {
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
