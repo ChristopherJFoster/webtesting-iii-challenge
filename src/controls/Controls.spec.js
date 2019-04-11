5941df4e9bfea80a218305357702e040a4083066
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-04-11 07:37:26 -0400

    After a lot of wrestling, I think I've written some good tests for the lock/unlock button specs in Controls.spec.js. I think I'm beginning to understand at least one use case for jest.fn().

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -4,13 +4,55 @@ import renderer from 'react-test-renderer';
 
 import Controls from './Controls';
 
-afterEach(() => {
-  cleanup();
-});
+beforeEach(cleanup);
 
 describe('<Controls />', () => {
   it('matches snapshot', () => {
     const tree = renderer.create(<Controls />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Lock/Unlock Button', () => {
+  it('diplays lock gate if gate is unlocked', () => {
+    const locked = false;
+    const { getByText } = render(<Controls locked={locked} />);
+    getByText(/^lock gate$/i);
+  });
+
+  it('diplays unlock gate if gate is locked', () => {
+    const locked = true;
+    const { getByText } = render(<Controls locked={locked} />);
+    getByText(/^unlock gate$/i);
+  });
+
+  it('is enabled if gate is closed', () => {
+    const locked = false;
+    const closed = true;
+    const mockToggleLocked = jest.fn();
+    const { getByText } = render(
+      <Controls
+        locked={locked}
+        closed={closed}
+        toggleLocked={mockToggleLocked}
+      />
+    );
+    fireEvent.click(getByText(/^lock gate$/i));
+    expect(mockToggleLocked).toHaveBeenCalledTimes(1);
+  });
+
+  it('is disabled if gate is opened', () => {
+    const locked = false;
+    const closed = false;
+    const mockToggleLocked = jest.fn();
+    const { getByText } = render(
+      <Controls
+        locked={locked}
+        closed={closed}
+        toggleLocked={mockToggleLocked}
+      />
+    );
+    fireEvent.click(getByText(/^lock gate$/i));
+    expect(mockToggleLocked).toHaveBeenCalledTimes(0);
   });
 });
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { render, fireEvent } from 'react-testing-library';
+import { render, fireEvent, cleanup } from 'react-testing-library';
 import renderer from 'react-test-renderer';
 
 import Dashboard from './Dashboard';
+
+beforeEach(cleanup);
 
 describe('<Dashboard />', () => {
   it('matches snapshot', () => {
@@ -14,14 +16,14 @@ describe('<Dashboard />', () => {
 describe('Lock/Unlock Button', () => {
   it('does not lock when gate is open', () => {
     const { getByText } = render(<Dashboard />);
-    fireEvent.click(getByText(/lock gate/i));
+    fireEvent.click(getByText(/^lock gate$/i));
     getByText(/^unlocked$/i);
   });
 
   it('locks when gate is closed', () => {
     const { getByText } = render(<Dashboard />);
-    fireEvent.click(getByText(/close gate/i));
-    fireEvent.click(getByText(/lock gate/i));
+    fireEvent.click(getByText(/^close gate$/i));
+    fireEvent.click(getByText(/^lock gate$/i));
     getByText(/^locked$/i);
   });
 });
@@ -29,15 +31,16 @@ describe('Lock/Unlock Button', () => {
 describe('Open/Close Button', () => {
   it('does not open when gate is locked', () => {
     const { getByText } = render(<Dashboard />);
-    fireEvent.click(getByText(/close gate/i));
-    fireEvent.click(getByText(/lock gate/i));
-    fireEvent.click(getByText(/open gate/i));
+    fireEvent.click(getByText(/^close gate$/i));
+    fireEvent.click(getByText(/^lock gate$/i));
+    fireEvent.click(getByText(/^open gate$/i));
     getByText(/^closed$/i);
   });
 
   it('opens when gate is unlocked', () => {
     const { getByText } = render(<Dashboard />);
-    fireEvent.click(getByText(/open gate/i));
+    fireEvent.click(getByText(/^close gate$/i));
+    fireEvent.click(getByText(/^open gate$/i));
     getByText(/^open$/i);
   });
 });
